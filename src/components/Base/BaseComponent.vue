86d1e1363efcd1bac0c16acc60d797511813f5a9
Zhiwu Mao <zwmao@outlook.com>
2022-08-08 16:07:14 +0800

    去除多余的注释

--- src/components/Base/BaseComponent.vue
+++ src/components/Base/BaseComponent.vue
@@ -11,9 +11,7 @@ export default {
   data() {
     return {}
   },
-  mounted() {
-    console.log(this)
-  },
+  mounted() {},
   created() {
     const interval = this.interval
     if (interval) {
