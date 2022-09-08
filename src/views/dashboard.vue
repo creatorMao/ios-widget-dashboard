c7a8f3dd8ede77f6d0289ffb714b1ed5986b6034
Zhiwu Mao <zwmao@outlook.com>
2022-09-08 22:55:14 +0800

    优化：组件改成全局注册

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -25,42 +25,11 @@
 </template>
 
 <script>
-import BaseComponent from '@/components/Base/BaseComponent.vue'
-import Group from '@/components/Base/Group.vue'
-import Battery from '@/components/Base/Battery.vue'
-import CountDown from '@/components/Date/CountDown.vue'
-import CommemorationDate from '@/components/Date/CommemorationDate.vue'
-import Time from '@/components/Date/Time.vue'
-import DownloadInfo from '@/components/Common/DownloadInfo.vue'
-import CommonCount from '@/components/Common/CommonCount.vue'
-import LineChart from '@/components/Common/LineChart.vue'
-import SwitchButton from '@/components/Common/SwitchButton.vue'
-import GitHubRepoStars from '@/components/Info/Github/GitHubRepoStars.vue'
-import GitHubRepoCloneTraffic from '@/components/Info/Github/GitHubRepoCloneTraffic.vue'
-import TwitterFollowers from '@/components/Info/Twitter/TwitterFollowers.vue'
-import TikTokDownloadStatus from '@/components/Info/TikTok/TikTokDownloadStatus.vue'
-import HeFengWeather from '@/components/Weather/HeFengWeather.vue'
 import { config } from '@/views/dashboardConfig.js'
 
 export default {
   name: 'DashBoard',
-  components: {
-    BaseComponent,
-    Group,
-    Battery,
-    DownloadInfo,
-    CountDown,
-    CommemorationDate,
-    Time,
-    CommonCount,
-    SwitchButton,
-    TikTokDownloadStatus,
-    TwitterFollowers,
-    GitHubRepoStars,
-    GitHubRepoCloneTraffic,
-    LineChart,
-    HeFengWeather
-  },
+  components: {},
   props: ['configId'],
   created() {
     this.config = config
