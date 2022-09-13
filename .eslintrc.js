11d2a3bc7f2e96b020190b2fd9a8821015091788
Zhiwu Mao <zwmao@outlook.com>
2022-09-13 18:20:00 +0800

    调整：去除组件名称必须复数规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    'vue/multi-word-component-names': 'warn'
+    'vue/multi-word-component-names': 'off'
   }
 }
