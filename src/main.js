e92e8d3d00b01d281608871acdb6e7f2a8962d89
Zhiwu Mao <zwmao@outlook.com>
2022-09-14 23:23:21 +0800

    优化：增加vuex

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import * as echarts from 'echarts'
 import './css/variable.less'
 import './css/common.less'
 import globalComponents from '@/components/index.js'
+import store from '@/store/index.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$dayjs = dayjs
@@ -18,5 +19,6 @@ Vue.use(globalComponents)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
